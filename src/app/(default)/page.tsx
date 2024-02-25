47e2a6106cfe8e57152cb4ec84b9b3ed5c62e614
MJoost <thiswallz@gmail.com>
2024-02-25 17:19:10 -0300

    copy changes

--- src/app/(default)/page.tsx
+++ src/app/(default)/page.tsx
@@ -10,7 +10,6 @@ import Features02 from '@/components/features-02'
 import Features03 from '@/components/features-03'
 import TestimonialsCarousel from '@/components/testimonials-carousel'
 import Features04 from '@/components/features-04'
-import Pricing from './pricing-section'
 import Testimonials from '@/components/testimonials'
 import Cta from '@/components/cta'
 
@@ -24,7 +23,6 @@ export default function Home() {
       <Features03 />
       <TestimonialsCarousel />
       <Features04 />
-      <Pricing />
       <Testimonials />
       <Cta />
     </>
--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -25,13 +25,13 @@ export default function Hero() {
               <div className="inline-flex relative before:absolute before:inset-0 before:bg-green-500 before:blur-md">
                 <a className="btn-sm py-0.5 text-slate-300 hover:text-white transition duration-150 ease-in-out group [background:linear-gradient(theme(colors.green.500),_theme(colors.green.500))_padding-box,_linear-gradient(theme(colors.green.500),_theme(colors.green.200)_75%,_theme(colors.transparent)_100%)_border-box] relative before:absolute before:inset-0 before:bg-slate-800/50 before:rounded-full before:pointer-events-none shadow" href="#0">
                   <span className="relative inline-flex items-center">
-                    API Studio is now in beta <span className="tracking-normal text-green-500 group-hover:translate-x-0.5 transition-transform duration-150 ease-in-out ml-1">-&gt;</span>
+                    Check Demo Page<span className="tracking-normal text-green-500 group-hover:translate-x-0.5 transition-transform duration-150 ease-in-out ml-1">-&gt;</span>
                   </span>
                 </a>
               </div>
             </div>
-            <h1 className="h1 bg-clip-text text-transparent bg-gradient-to-r from-slate-200/60 via-slate-200 to-slate-200/60 pb-4" data-aos="fade-down">The API Security Framework</h1>
-            <p className="text-lg text-slate-300 mb-8" data-aos="fade-down" data-aos-delay="200">Our landing page template works on all devices, so you only have to set it up once, and get beautiful results forever.</p>
+            <h1 className="h1 bg-clip-text text-transparent bg-gradient-to-r from-slate-200/60 via-slate-200 to-slate-200/60 pb-4" data-aos="fade-down">Unlock SaaS Engagement</h1>
+            <p className="text-lg text-slate-300 mb-8" data-aos="fade-down" data-aos-delay="200">Gain extra lead from your SaaS lovely customers with nocode for <span className='text-pink-400 animate-bounce'>free</span> </p>
             <div className="max-w-xs mx-auto sm:max-w-none sm:inline-flex sm:justify-center space-y-4 sm:space-y-0 sm:space-x-4" data-aos="fade-down" data-aos-delay="400">
               <div>
                 <a className="btn text-slate-900 bg-gradient-to-r from-white/80 via-white to-white/80 hover:bg-white w-full transition duration-150 ease-in-out group" href="#0">
